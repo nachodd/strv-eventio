0eac2e2f826a738a8c5c8bf999f30dc272fcdef6
nachodd <nachodurand@gmail.com>
2019-01-31 21:12:33 -0300

    initing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Router } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router basename="/">
+        <Switch>
+          <Route exact path="/login" component={ Login } />
+          {/*<Route exact path="/signup" component={ Signup } />*/}
+          {/*<Route exact path="/login" component={ Login } />*/}
+          {/*<Route exact path="/forgotpassword" component={ ForgotPassword } />*/}
+          {/*<Route exact path="/forgotpassword/sent" component={ PasswordSent } />*/}
+          {/*<Route component={ NotFound } />*/}
+        </Switch>
+      </Router>
     );
   }
 }
